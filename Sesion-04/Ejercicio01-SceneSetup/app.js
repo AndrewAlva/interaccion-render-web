af29137e279613d167a262295225ef1f5342db2f
Andrew Alvarado <andrew@activetheory.net>
2025-08-26 13:45:12 -0600

    New sesion: 04; first example for class: 01 scene setup.

---
+++ Sesion-04/Ejercicio01-SceneSetup/app.js
@@ -0,0 +1,38 @@
+console.log("S04. Ejercicio 01: Scene Setup");
+console.log(THREE);
+
+////////////////////
+// 1. Canvas setup.
+const canvas = document.getElementById("lienzo");
+var width = window.innerWidth;
+var height = window.innerHeight;
+
+
+////////////////////
+// 2. 3D Scene setup.
+// Renderer.
+const renderer = new THREE.WebGLRenderer({ canvas: canvas, });
+renderer.setSize(width, height);
+
+// Scene.
+const scene = new THREE.Scene();
+
+// Camera.
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+camera.position.z = 2;
+
+// Object or Mesh.
+const mesh = new THREE.Mesh(
+    new THREE.SphereGeometry(),
+    new THREE.MeshBasicMaterial({ color: "#ff6600", wireframe: true })
+);
+
+
+////////////////////////////////////////
+// 3. Add object to the scene.
+scene.add(mesh);
+
+
+////////////////////////////////////////
+// 4. Get renderer to render the scene.
+renderer.render(scene, camera);
